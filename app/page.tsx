2b00c75a8f8ba2507b2e150ae8190b4155b2fce8
mattiabenvenuti90 <56254673+welcomemarketing@users.noreply.github.com>
2024-10-02 22:54:20 +0200

    aggiornato testo home

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
           className={styles.shape}
         />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to my Project. You can program something!</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
