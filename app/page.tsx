fe30920637a73580e2e2244b66b1c1b5d8ff2568
mattiabenvenuti90 <56254673+welcomemarketing@users.noreply.github.com>
2024-10-02 23:01:38 +0200

    aggiornato nuovamente testo home

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,7 @@ export default function Page() {
           className={styles.shape}
         />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to my Project. You can program something!</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to my Project. You can program something!</strong>
           </p>
           <Link
             href="/login"
